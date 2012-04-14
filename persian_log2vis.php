7fa53e58a1c07de8730624f66888ca2431311d23
Omid Mottaghi <omidmr@gmail.com>
2012-04-14 11:10:00 +0430

    make it compatible with PHP5.4!

--- persian_log2vis.php
+++ persian_log2vis.php
@@ -15,30 +15,32 @@
 /**
  * A function to change persian or arabic text from its logical condition to visual
  *
- * @author		Omid Mottaghi Rad 
- * @param		string	Main text you want to change it
- * @param		boolean	Apply e'raab characters or not? default is true
- * @param		boolean	Which encoding? default it "utf8"
- * @param		boolean	Do you want to change special characters like "allah" or "lam+alef" or "lam+hamza", default is true
+ * @author        Omid Mottaghi Rad 
+ * @param        string    Main text you want to change it
+ * @param        boolean    Apply e'raab characters or not? default is true
+ * @param        boolean    Which encoding? default it "utf8"
+ * @param        boolean    Do you want to change special characters like "allah" or "lam+alef" or "lam+hamza", default is true
  */
 function persian_log2vis(&$str)
 {
-	include('bidi.php');
-	
+    include('bidi.php');
+    
+    $bidi = new bidi();
+    
     $text = explode("\n", $str);
-	
-	$str = array();
-	
-	foreach($text as $line){
-		$chars = bidi::utf8Bidi(bidi::UTF8StringToArray($line), 'R');
-		$line = '';
-		foreach($chars as $char){
-			$line .= bidi::unichr($char);
-		}
-		
-		$str[] = $line;
-	}
-	
-	$str = implode("\n", $str);
+    
+    $str = array();
+    
+    foreach($text as $line){
+        $chars = $bidi->utf8Bidi($bidi->UTF8StringToArray($line), 'R');
+        $line = '';
+        foreach($chars as $char){
+            $line .= $bidi->unichr($char);
+        }
+        
+        $str[] = $line;
+    }
+    
+    $str = implode("\n", $str);
 }
 ?>
